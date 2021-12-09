44a66d64192ee65622ff429d78de6db50ca3ed10
asifs-cse <asifs.cse@gmail.com>
2021-12-10 01:05:17 +0530

    value swaping

--- pointerSwaping.c
+++ pointerSwaping.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a=100, b=20, *p1=&a, *p2=&b;
-    printf("Before swap: *p1=%d *p2=%d",*p1,*p2);  
-    *p1 +=*p2;
-    *p2 =*p1-*p2;
-    *p1 -=*p2;
-    printf("\nAfter swap: *p1=%d *p2=%d",*p1,*p2); 
+    int a=100, b=20, p1=a, p2=b;
+    printf("Before swap: *p1=%d *p2=%d",p1,p2);  
+    p1 +=p2;
+    p2 =p1-p2;
+    p1 -=p2;
+    printf("\nAfter swap: *p1=%d *p2=%d",p1,p2); 
     return 0;
 }
