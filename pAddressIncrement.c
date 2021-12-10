f9323908f534619854502290b346099ff8d2f48a
asifs-cse <asifs.cse@gmail.com>
2021-12-10 06:43:52 +0530

    factorial using recoursion

--- pAddressIncrement.c
+++ pAddressIncrement.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
-    int *num=100;
-    printf("this variable address of: %x", num+1);
-    printf("\nthis variable value of: %d", num);
+    int a=90;
+    int *num=&a;
+    printf("this variable address of: %x", num);
+    printf("\nthis variable value of: %d", *num);
 
     return 0;
 }
