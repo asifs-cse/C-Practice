4d209e8b1711c920a341101dd126a6ff64fe4391
asifs-cse <asifs.cse@gmail.com>
2021-12-11 23:04:45 +0530

    revers number

--- reverseNumber.c
+++ reverseNumber.c
@@ -3,7 +3,7 @@ int reversDigits(int num){
     int rev_num = 0;
     while (num>0)
     {
-        rev_num = rev_num*10+num%10;
+        rev_num = rev_num*10+num%10; //rev
         num = num/10;
     }
     return rev_num;
