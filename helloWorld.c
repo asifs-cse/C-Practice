9622d9ff868f5e9898e508bd8d57657b36d3bf75
asifs-cse <asifs.cse@gmail.com>
2021-12-12 23:29:56 +0530

    input

--- helloWorld.c
+++ helloWorld.c
@@ -3,7 +3,7 @@ int main(){
     printf("Hello world"); //print
     int num;
     printf("Enter input: ");
-    scanf("%d",&num);
+    scanf("%d",&num); //input
     printf("Answer: %d",num);
     return 0;
 }
