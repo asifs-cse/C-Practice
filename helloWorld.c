ad114737e4c4e9c1a94720d34035af328f96bcf9
asifs-cse <asifs.cse@gmail.com>
2021-12-12 23:28:46 +0530

    input and output

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(){
     printf("Hello world"); //print
+    int num;
+    printf("Enter input: ");
+    scanf("%d",&num);
+    printf("Answer: %d",num);
     return 0;
 }
