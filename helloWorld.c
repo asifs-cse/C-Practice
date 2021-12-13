80ff30fde8850ad50dd43c9fab7559fe07d2d102
asifs-cse <asifs.cse@gmail.com>
2021-12-14 00:02:09 +0530

    return

--- helloWorld.c
+++ helloWorld.c
@@ -5,5 +5,5 @@ int main(){
     printf("Enter input: "); //print message
     scanf("%d",&num); //input data
     printf("Your number is: %d",num); //output data
-    return 0; //return value
+    return 0; //return 0
 }
