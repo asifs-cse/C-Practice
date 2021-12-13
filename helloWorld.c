9fc202c0f4f235a8e0aa0a97f408e4b1a33aed9b
asifs-cse <asifs.cse@gmail.com>
2021-12-14 00:00:00 +0530

    variable

--- helloWorld.c
+++ helloWorld.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     printf("Hello world"); //print data
-    int num;
+    int num; //variable
     printf("Enter input: ");
     scanf("%d",&num); //input data
     printf("Your number is: %d",num); //output data
