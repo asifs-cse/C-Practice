2ed6b5eead544621915517ec2eb49a9ae54584cd
asifs-cse <asifs.cse@gmail.com>
2021-12-14 00:01:04 +0530

    output message

--- helloWorld.c
+++ helloWorld.c
@@ -2,7 +2,7 @@
 int main(){
     printf("Hello world"); //print data
     int num; //variable
-    printf("Enter input: ");
+    printf("Enter input: "); //print message
     scanf("%d",&num); //input data
     printf("Your number is: %d",num); //output data
     return 0;
